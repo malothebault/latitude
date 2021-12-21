8dfb63c2bc837cb0a68fcdf5d4a580c0732b0114
Malo <malothebault@lilo.org>
2021-12-22 00:35:32 +0100

    add vbox

--- src/window.py
+++ src/window.py
@@ -18,16 +18,39 @@
     along with ElementaryPython.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import gi
+import locale
+import os
+import gettext
+import constants as cn
 
 gi.require_version('Gtk', '3.0')
 # gi.require_version('Granite', '1.0')
 
 from gi.repository import Gtk
 
-import constants as cn
-import headerbar as hb
+# import constants as cn
+# import headerbar as hb
 # import welcome as wl
-import stack as sk
+# import stack as sk
+
+########### TRANSLATION ##############
+try:
+    current_locale, encoding = locale.getdefaultlocale()
+    locale_path = os.path.join(
+        os.path.abspath(
+            os.path.dirname(__file__)
+        ),
+        'locale'
+    )
+    translate = gettext.translation(
+        cn.App.application_shortname,
+        locale_path,
+        [current_locale]
+    )
+    _ = translate.gettext
+except FileNotFoundError:
+    _ = str
+######################################
 
 class Window(Gtk.Window):
 
@@ -36,6 +59,7 @@ class Window(Gtk.Window):
             self, 
             title=cn.App.application_name
         )
+        self._ = _
         
         self.main_file = {"name": "", "path": ""}
         
@@ -44,14 +68,40 @@ class Window(Gtk.Window):
 
         '''Here we are creating a new instance of Headerbar 
         and setting as the titlebar'''
-        self.hbar = hb.Headerbar(self)
-        self.set_titlebar(self.hbar)
+        # self.hbar = hb.Headerbar(self)
+        # self.set_titlebar(self.hbar)
+        self.headerbar = Gtk.HeaderBar(decoration_layout_set=True, decoration_layout="close:")
+        headerbar_context = self.headerbar.get_style_context()
+        headerbar_context.add_class("flat")
+        self.headerbar.set_show_close_button(True)
+        self.headerbar.props.title = cn.App.application_name
+        #self.headerbar.set_custom_title("Latitude")
+        self.set_titlebar(self.headerbar)
 
-        '''This is a new instance of Welcome, we are 
-        simply adding it to the window
-        Note: Not all applications need a Welcome Page, make 
-        your choice well.'''
-        # self.welcome = wl.Welcome()
-        # self.add(self.welcome)
-        self.stack = sk.Stack(self)
-        self.add(self.stack)
+        self.vbox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 20, homogeneous = False, valign = Gtk.Align.CENTER)
+        vbox_context = self.vbox.get_style_context()
+        vbox_context.add_class("main_content")
+
+        self.select_file_button = Gtk.Button(label=_("Select File"),image=Gtk.Image(icon_name="document-open-symbolic", icon_size=Gtk.IconSize.BUTTON), always_show_image=True, can_focus=False)
+        self.select_file_button.connect("clicked", self.main_file_selection)
+        select_file_button_context = self.select_file_button.get_style_context()
+        select_file_button_context.add_class("suggested-action")
+        
+        self.simple_button = Gtk.Button.new_with_label("text")
+
+        self.vbox.pack_start(self.select_file_button, False, False, 0)
+        self.vbox.pack_start(self.simple_button, False, False, 0)
+        
+        self.add(self.vbox)
+        
+        
+        
+        
+    def main_file_selection(self, button):
+        dialog = Gtk.FileChooserNative.new(_("Please choose a file"), self, Gtk.FileChooserAction.OPEN, _("Open"), _("Cancel"))
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.ACCEPT:
+                print("Hello")
+
+        dialog.destroy()
